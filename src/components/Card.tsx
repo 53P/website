f28dcac11d2aa55d0772c0f60ee53f0300438aa5
53P <53p.playz@gmail.com>
2023-07-22 10:58:19 -0600

    feat: changes

---
+++ src/components/Card.tsx
@@ -0,0 +1,7 @@
+const Card = (): JSX.Element => {
+	return (
+		<>
+
+		</>
+	);
+};
---
+++ src/pages/How.tsx
@@ -0,0 +1,21 @@
+import { useEffect, useState } from 'react';
+import { motion } from 'framer-motion';
+import {
+	SiGithub,
+	SiTwitter,
+} from 'react-icons/si';
+import { Link, useLocation } from 'react-router-dom';
+import styled from 'styled-components';
+
+const How = (): JSX.Element  => {
+	const { pathname } = useLocation();
+
+	return (
+		<>
+			<p>hi</p>
+		</>
+	);
+};
+
+
+export default How;
